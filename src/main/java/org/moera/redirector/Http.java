3a185ac18267335ee009c9eeaf84e9211f192b03
Shmuel Melamud <smelamud@redhat.com>
2024-01-24 04:26:22 +0200

    UI for changing the default client.

--- src/main/java/org/moera/redirector/Http.java
+++ src/main/java/org/moera/redirector/Http.java
@@ -1,18 +1,46 @@
 package org.moera.redirector;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Http {
 
     public static final int OK = 200;
+    public static final int SEE_OTHER = 303;
     public static final int TEMPORARY_REDIRECT = 307;
     public static final int BAD_REQUEST = 400;
     public static final int NOT_FOUND = 404;
     public static final int METHOD_NOT_ALLOWED = 405;
     public static final int PAYLOAD_TOO_LARGE = 413;
+    public static final int INTERNAL_SERVER_ERROR = 500;
 
     public static final String COOKIE = "cookie";
     public static final String SET_COOKIE = "set-cookie";
     public static final String USER_AGENT = "user-agent";
 
     public static final String COOKIE_CLIENT = "client";
+
+    public static Map<String, String> parseQuery(String query) {
+        if (query == null) {
+            return Collections.emptyMap();
+        }
+        if (query.startsWith("?")) {
+            query = query.substring(1);
+        }
+        if (query.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        return Arrays.stream(query.split("&"))
+                .map(p -> p.split("="))
+                .collect(Collectors.toMap(
+                        p -> p[0],
+                        p -> URLDecoder.decode(p[1], StandardCharsets.UTF_8),
+                        (v1, v2) -> v2
+                ));
+    }
 
 }
--- src/main/java/org/moera/redirector/SetClientHandler.java
+++ src/main/java/org/moera/redirector/SetClientHandler.java
@@ -1,28 +1,95 @@
 package org.moera.redirector;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SetClientHandler implements HttpHandler {
 
-    public static final int MAX_LENGTH = 256;
+    private static final Logger log = LoggerFactory.getLogger(SetClientHandler.class);
+
+    public static final int MAX_LENGTH = 1024;
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        if (!httpExchange.getRequestMethod().equalsIgnoreCase("POST")) {
-            httpExchange.sendResponseHeaders(Http.METHOD_NOT_ALLOWED, 0);
+        switch (httpExchange.getRequestMethod().toUpperCase()) {
+            case "GET":
+                get(httpExchange);
+                break;
+            case "POST":
+                post(httpExchange);
+                break;
+            default:
+                httpExchange.sendResponseHeaders(Http.METHOD_NOT_ALLOWED, 0);
+                httpExchange.close();
+        }
+    }
+
+    private void get(HttpExchange httpExchange) throws IOException {
+        Map<String, String> params = Http.parseQuery(httpExchange.getRequestURI().getQuery());
+        if (!params.containsKey("client")) {
+            httpExchange.sendResponseHeaders(Http.BAD_REQUEST, 0);
             httpExchange.close();
+            return;
         }
 
-        String clientDomain = new String(httpExchange.getRequestBody().readNBytes(MAX_LENGTH), StandardCharsets.UTF_8);
+        InputStream input = getClass().getClassLoader().getResourceAsStream("set-client.html");
+        if (input == null) {
+            log.error("Cannot load HTML template");
+            httpExchange.sendResponseHeaders(Http.INTERNAL_SERVER_ERROR, 0);
+            httpExchange.close();
+            return;
+        }
+
+        String html = readAll(input);
+        html = html.replace("{{client}}", params.get("client"));
+        String returnHref = params.get("return");
+        if (returnHref == null || returnHref.isEmpty()) {
+            returnHref = "#";
+        }
+        html = html.replace("{{return}}", returnHref);
+
+        httpExchange.sendResponseHeaders(Http.OK, 0);
+
+        Writer writer = new OutputStreamWriter(httpExchange.getResponseBody());
+        writer.write(html);
+        writer.flush();
+        writer.close();
+
+        httpExchange.close();
+    }
+
+    private static String readAll(InputStream input) throws IOException {
+        StringBuilder buf = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buf.append(line);
+        }
+        return buf.toString();
+    }
+
+    private void post(HttpExchange httpExchange) throws IOException {
+        Map<String, String> params = Http.parseQuery(
+                new String(httpExchange.getRequestBody().readNBytes(MAX_LENGTH), StandardCharsets.UTF_8)
+        );
         if (httpExchange.getRequestBody().available() > 0) {
             httpExchange.sendResponseHeaders(Http.PAYLOAD_TOO_LARGE, 0);
             httpExchange.close();
         }
-        if (!clientDomain.matches("[a-zA-Z0-9-.]+")) {
+
+        String clientDomain = params.get("client");
+        if (clientDomain == null || !clientDomain.matches("[a-zA-Z0-9-.]+")) {
             httpExchange.sendResponseHeaders(Http.BAD_REQUEST, 0);
             httpExchange.close();
         }
@@ -35,7 +102,14 @@ public class SetClientHandler implements HttpHandler {
                         100L * 365 * 24 * 60 * 60
                 )
         );
-        httpExchange.sendResponseHeaders(Http.OK, 0);
+
+        String returnHref = params.get("return");
+        if (returnHref != null && !returnHref.isEmpty()) {
+            httpExchange.getResponseHeaders().add("Location", returnHref);
+            httpExchange.sendResponseHeaders(Http.SEE_OTHER, 0);
+        } else {
+            httpExchange.sendResponseHeaders(Http.OK, 0);
+        }
         httpExchange.close();
     }
 
